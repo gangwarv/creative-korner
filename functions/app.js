0cd87631f2d31c6afb7e72a93f0ab739094d262f
Vishal Gangwar <vikash4654@gmail.com>
2020-06-30 00:26:21 +0530

    hosting done

--- functions/app.js
+++ functions/app.js
@@ -1,35 +1,21 @@
 const express = require("express");
-const SSREngine = require("./view-engine");
+const path = require("path");
+const fs = require("fs");
 
-var path = require("path");
-var fs = require("fs");
 const app = express();
-const port = 3000;
 
-SSREngine(app);
+var indexPage = null;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public", "index.html"))
-);
-// app.get("/blog/*", (req, res) =>
-//   res.render("index")
-// );
-
-app.get("/blog/*", function (req, res, next) {
+app.get("/*", function (req, res, next) {
   if (req.path.includes(".")) return res.sendStatus(404);
+
   console.log("/*", req.path);
-    // fs.readFile(__dirname + "/public/index.html", function(err, data) {
-    //     res.writeHead(200, {'Content-Type': 'text/html'});
-    //     console.log(data)
-    //     res.write(data.replace('{}','[]'));
-    //     return res.end();
-    //   });
-    var file = fs.readFileSync(__dirname + "/public/index.html")
-    res.send(file.toString().replace('<!--SSR-->','<!--NO ENGINE-->'))
+  if (!indexPage) indexPage = fs.readFileSync(__dirname + "/public/index1.html");
+
+  res.send(indexPage.toString().replace("<!--SSR-->", "<!--META--TAGS-->"));
+
 });
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+module.exports = app;
---
+++ src/app/my-stories/my-stories.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MyStoriesRoutingModule } from './my-stories-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MyStoriesRoutingModule
+  ]
+})
+export class MyStoriesModule { }
